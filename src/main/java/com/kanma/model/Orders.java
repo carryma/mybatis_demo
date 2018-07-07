f4cd874ab75761d500023119548e04d16eb18059
Kang Ma <kanma.cn@gmail.com>
2018-07-08 01:36:24 +0800

    add java model based mysql table

---
+++ src/main/java/com/kanma/model/Orders.java
@@ -0,0 +1,102 @@
+package com.kanma.model;
+
+import java.sql.Date;
+import java.util.List;
+
+/**
+ * @ Desc   ：Orders 表
+ * @ Author ：MaKang
+ * @ Date   ：Created in 2018/7/8 1:17
+ */
+
+/*
+ CREATE TABLE `orders` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `user_id` int(11) NOT NULL COMMENT '下单用户id',
+    `number` varchar(32) NOT NULL COMMENT '订单号',
+    `createtime` datetime NOT NULL COMMENT '创建订单时间',
+    `note` varchar(100) DEFAULT NULL COMMENT '备注',
+    PRIMARY KEY (`id`),
+    KEY `FK_orders_1` (`user_id`),
+    CONSTRAINT `FK_orders_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+*/
+
+public class Orders {
+    private Integer id;
+
+    private Integer userId;
+
+    private String number;
+
+    private Date createtime;
+
+    private String note;
+
+    //用户信息
+    private User user;
+
+    //订单明细
+    private List<Orderdetail> orderdetails;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number == null ? null : number.trim();
+    }
+
+    public Date getCreatetime() {
+        return createtime;
+    }
+
+    public void setCreatetime(Date createtime) {
+        this.createtime = createtime;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note == null ? null : note.trim();
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<Orderdetail> getOrderdetails() {
+        return orderdetails;
+    }
+
+    public void setOrderdetails(List<Orderdetail> orderdetails) {
+        this.orderdetails = orderdetails;
+    }
+
+    //public void print(){
+    //    System.out.println("----test-print-----"+user+"   user==null: "+(user==null));
+    //}
+
+}
